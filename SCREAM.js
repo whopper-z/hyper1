698ba9b70ab27c25671654a317bdfb64378221f9
whopper-z <79403251+whopper-z@users.noreply.github.com>
2021-10-22 20:04:28 +0300

    Add files via upload

---
+++ SCREAM.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+
+const scream = new Discord.Client();
+const scream1 = new Discord.Client();
+const scream2 = new Discord.Client();
+const scream3 = new Discord.Client();
+const scream4 = new Discord.Client();
+const scream5 = new Discord.Client();
+scream1.on('ready', () => {
+
+scream1.channels.get('883502883457359902').join()
+})
+
+scream2.on('ready', () => {
+
+scream2.channels.get('883502883457359902').join()
+})
+
+scream3.on('ready', () => {
+
+scream3.channels.get('883502883457359902').join()
+})
+
+scream4.on('ready', () => {
+
+scream4.channels.get('883502883457359902').join()
+})
+
+
+scream1.login("ODg3NjE0NDc3Njk1MDgyNTA3.YUGtkQ.QvDGiFhlLxzGskiz4ZXBNoS7pg4");
+scream2.login("ODg3NjE2MjM4NzQwNzI1Nzcw.YUGvQg.94BG29B4pka_SKrNFo2KExdjSM4");
+scream3.login("ODg3NjE2OTM0NTc1NzM4ODkw.YUGv0A.mbSkTf-It_biCFjE5_4dnfFJYOM");
+scream4.login("ODg3NjE3ODIxODY4MTY3MTY4.YUGwjQ.huPG5xgCRIYEbSja8kZpdtoIrVg");
+scream5.login("ODg3NjE4NTEzMTM5ODI2NzE4.YUGxRQ.nNImXZk1h0netzCpFtCpeDwnE3w");
